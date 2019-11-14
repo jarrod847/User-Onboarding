898562b89e94331944c19b6f298059a7489087a7
jarrodskahill <jarrodskahill@gmail.com>
2019-11-14 12:23:40 -0800

    started adding the code to the form and exported it to App

--- wizard/src/Components/Form.js
+++ wizard/src/Components/Form.js
@@ -1,10 +1,39 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 
-function Form() {
+
+const Login = ({values}) => {
     return(
-        <div></div>
+        <div>
+            <h2>Login</h2>
+            <Form>
+            <label className="userinfo">Name
+            <Field type="text" name="name" placeholder="insert name"/>
+            </label>
+            <label className="userinfo">Email
+            <Field type="text" name="email" placeholder="insert email"/>
+            </label>
+            <label className="userinfo">Password
+            <Field type="text" name="pw" placeholder="insert password"/>
+            </label>
+            <button type="submit">Submit</button>
+            </Form>
+        </div>
     )
 }
 
-export default Form
+const FormikLogin = withFormik({
+    mapPropsToValues({name, email, pw}){
+        return{
+            name: name || "",
+            email: email || "",
+            pw: pw || ""
+        }
+    }
+})(Login);
+
+
+
+export default FormikLogin
